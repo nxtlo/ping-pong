20ef5030410b7121d1e51f7a5aa2df90c5b400c1
nxtlo <35225215+nxtlo@users.noreply.github.com>
2020-10-11 23:25:08 +0300

    Delete discord.js

--- discord.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-client.on('message', message => {
-    if (message.content === '+ping') {  
-      message.channel.send('Pong!');
-    }
-  });
