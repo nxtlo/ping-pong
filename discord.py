b1e9a0fd3107c0c586d68c63bbcb936e9c8db865
fate <35225215+nxtlo@users.noreply.github.com>
2020-10-13 19:08:15 +0300

    Update discord.py
    add exceptions for errors and how to use the command in a normal bot file **not cog**

--- discord.py
+++ discord.py
@@ -6,19 +6,33 @@ class ping(commands.Cog):
         self.bot = bot
 
 
-# commands
+# commands in a cog
 
     @commands.command(name='ping', description='ping command')
     async def ping(self, ctx):
         await ctx.send('pong!')
 
-# listeners
+# listeners in a cog
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if '!ping' in message.content:
             await message.channel.send('pong!')
 
+ # in main.bot with exceptions
+ 
+@bot.command(aliases=['latency'], description="show bot's pin")
+async def ping(ctx):
+    try:
+        await ctx.send("Pong!")
+    
+    except Exception:
+        await ctx.send("command wan not found, perhaps you meant ping?")
+    
+    # raise the error to our shell
+    else:
+        raise
+       
 
 # you must register the cogs to be used by typing this.
 
