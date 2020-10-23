5f7b75e05dcf75593d52e331a5e403406c1c3746
fate <dhmony-99@hotmail.com>
2020-10-23 07:25:32 +0300

    fixed client -> bot

--- discord.js
+++ discord.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-client.on('message', message => {
+bot.on('message', message => {
     if (message.content === '+ping') {  
       message.channel.send('Pong!');
     }
